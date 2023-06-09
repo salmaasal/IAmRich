1e8031bf7422ac0518ad7391dc404b4aa58b5746
salma <salameri482@gmail.com>
2023-06-09 09:58:48 +0400

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by MAC008634 on 08/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
